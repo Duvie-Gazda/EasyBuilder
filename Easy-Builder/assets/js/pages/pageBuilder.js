8f96acba556c91cd523e7bb36665ddbac5d089ff
Andryi-Melashenko <andryimelashenko@gmail.com>
2020-08-14 02:54:31 +0300

    New studio version

--- Easy-Builder/assets/js/pages/pageBuilder.js
+++ Easy-Builder/assets/js/pages/pageBuilder.js
@@ -1,76 +1,76 @@
-class elementOnPanel{
-    constructor (type,content, elementClass,id, appendTo){
-        this.type = type;
-        this.content = content;
-        this.elementClass = elementClass;
-        this.appendTo = appendTo;
-        this.id = id;
-    }
-}
-function appendElement(element){
-    let num = 0;
-    for(let data of element){
-        let id = data.id;
-        if ((id == '')||(id == 'undefind')){
-            id = 'idElement'+num+'EasyBuilder';
-        }
-        num++;
-        //add Elements to Extra panel
+// class elementOnPanel{
+//     constructor (type,content, elementClass,id, appendTo){
+//         this.type = type;
+//         this.content = content;
+//         this.elementClass = elementClass;
+//         this.appendTo = appendTo;
+//         this.id = id;
+//     }
+// }
+// function appendElement(element){
+//     let num = 0;
+//     for(let data of element){
+//         let id = data.id;
+//         if ((id == '')||(id == 'undefind')){
+//             id = 'idElement'+num+'EasyBuilder';
+//         }
+//         num++;
+//         //add Elements to Extra panel
 
-        let elementik = $(data.type).addClass(data.elementClass).attr('id',id);
-        elementik.html(data.content).appendTo(data.appendTo == undefined? '#extra-panel-EasyBuilder':data.appendTo);
-    }
-}
+//         let elementik = $(data.type).addClass(data.elementClass).attr('id',id);
+//         elementik.html(data.content).appendTo(data.appendTo == undefined? '#extra-panel-EasyBuilder':data.appendTo);
+//     }
+// }
 
-function classToggler(elementName,classToggleArr, mainClassArr){
-    let element = $(elementName);
-    for(let classToggle of classToggleArr)
-    {
+// function classToggler(elementName,classToggleArr, mainClassArr){
+//     let element = $(elementName);
+//     for(let classToggle of classToggleArr)
+//     {
 
-        if(element.hasClass(classToggle)){
-            element.removeClass();
-            for(let data of mainClassArr){
-                element.addClass(data);
-            }
-        }
-        else{
-            element.removeClass();
-            for(let data of mainClassArr){
-                element.addClass(data);
-            }
-            for(let data of classToggleArr){
-                element.addClass(data);
-            }
-        }
-    }
-}
+//         if(element.hasClass(classToggle)){
+//             element.removeClass();
+//             for(let data of mainClassArr){
+//                 element.addClass(data);
+//             }
+//         }
+//         else{
+//             element.removeClass();
+//             for(let data of mainClassArr){
+//                 element.addClass(data);
+//             }
+//             for(let data of classToggleArr){
+//                 element.addClass(data);
+//             }
+//         }
+//     }
+// }
 
-function addDataByClick(clickedElementName , element, parnetName, extraClasses, mainClass){
-    // Extra Panel Initalization
-    let parent = $(parnetName);
-    // Clicked element initialization
-        parent.empty();
-        // if Extra panel is not Empty 
+// function addDataByClick(clickedElementName , element, parnetName, extraClasses, mainClass){
+//     // Extra Panel Initalization
+//     let parent = $(parnetName);
+//     // Clicked element initialization
+//         parent.empty();
+//         // if Extra panel is not Empty 
 
-        if (parent.hasClass(clickedElementName+'clicked')){
-            // if it not empty Empty it!
-            parent.removeClass();
-            parent.addClass(mainClass);
-        }
-        else{
-            //remove connect class
-            parent.removeClass();
-            //add All classes
-            parent.addClass(mainClass);
-            for(let clas of extraClasses){
-                parent.addClass(clas);
-            }
-            //add class clicked element
-            parent.addClass(clickedElementName+'clicked');
-            // add data
-            appendElement(element);
-        }
-}
+//         if (parent.hasClass(clickedElementName+'clicked')){
+//             // if it not empty Empty it!
+//             parent.removeClass();
+//             parent.addClass(mainClass);
+//         }
+//         else{
+//             //remove connect class
+//             parent.removeClass();
+//             //add All classes
+//             parent.addClass(mainClass);
+//             for(let clas of extraClasses){
+//                 parent.addClass(clas);
+//             }
+//             //add class clicked element
+//             parent.addClass(clickedElementName+'clicked');
+//             // add data
+//             appendElement(element);
+//         }
+// }
 
 
 //ELements show on Extra Panel
@@ -140,16 +140,6 @@ $('#task-EasyBuilder').on('click', function(){
 // preview show on extra panel
 $('#devices-EasyBuilder').on('click', function(){
     let data = [];
-    // data.push(new elementOnPanel('<div>','','row','devices-preview','#extra-panel-EasyBuilder'));
-    // data.push(new elementOnPanel('<div>','','row','devices-preview','#extra-panel-EasyBuilder'));
-
-    // //smartphone wertical 
-    // data.push(new elementOnPanel('<div>','','preview-row','smartphone-preview','#extra-panel-EasyBuilder'));
-    // data.push(new elementOnPanel('<div>','smartphone','previesw-card material-icons unselectable','smartphone-card','#smartphone-preview'));
-    
-    // // horizontal
-    // data.push(new elementOnPanel('<div>','','preview-row','smartphone-preview','#extra-panel-EasyBuilder'));
-    // data.push(new elementOnPanel('<div>','tablet','previesw-card material-icons unselectable','horizontal-card','#smartphone-preview'));
     
     data.push(new elementOnPanel('<DIV>','','row','row'))
     data.push(new elementOnPanel('<DIV>','smartphone','material-icons col-lg-5 element-card-extra-panel unselectable','smartphone-card','#row'));
@@ -167,24 +157,11 @@ $('#devices-EasyBuilder').on('click', function(){
     }
 })
 
-//shop show on shop popup
-// $('#shop-EasyBuilder').on('click', function(){
-//     let data = [];
-//     data.push(new elementOnPanel('<DIV>','','row','row','#shop-popup'))
-//     data.push(new elementOnPanel('<DIV>','smartphone','material-icons col-lg-5 element-card-extra-panel unselectable','smartphone-card','#row'));
-//     data.push(new elementOnPanel('<div>','tablet','material-icons col-lg-5 element-card-extra-panel unselectable','horizontal-card','#row'));
-
-//     //add elements
-//     addDataByClick('#shop-EasyBuilder',data,'#shop-popup',['content-shop-popup'],'shop-popup');
-// })
+//shop Easy Builder
 $('#shop-EasyBuilder').on('click', function(){
     let data = [];
-    //close button
-    // data.push(new elementOnPanel('<DIV>','','row ','row-1','#shop-popup'));
-    // data.push(new elementOnPanel('<DIV>','','col-lg-11 unselectable','','#row-1'));
-    // data.push(new elementOnPanel('<DIV>','close','col-lg-1 unselectable material-icons close-popup','btn-close','#row-1'));
 
-    // shop panel
+
     data.push(new elementOnPanel('<DIV>','','row','shopik','#shop-popup'));
     
     data.push(new elementOnPanel('<DIV>','','col-lg-3 buttons-shop','buttons-shop','#shopik'));
@@ -192,7 +169,6 @@ $('#shop-EasyBuilder').on('click', function(){
     data.push(new elementOnPanel('<DIV>','','col-lg-9','','#sidebar-row-1'));
     data.push(new elementOnPanel('<DIV>','close','col-lg-3 material-icons unselectable','btn-close','#sidebar-row-1'));
 
-    //data.push(new elementOnPanel('<div>','Elements','btn-info','elements-btn-EasyBuilder','#buttons-shop'))
 
     data.push(new elementOnPanel('<DIV>','','col-lg-9 shop-element-container','shop-elemetns-container','#shopik'));
 
